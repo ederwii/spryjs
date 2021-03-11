b1e8c1720149e6006a2d03156008273bc1a36748
Alan Martinez <eder.marquez@filmtrack.net>
2021-03-11 14:07:52 -0600

    Identity code refactor

--- src/data/identity.model.ts
+++ src/data/identity.model.ts
@@ -4,16 +4,12 @@ import IBaseEntity from "../base/base.entity";
 export interface IUser extends IBaseEntity {
   username: string;
   password: string;
-  firstName: string;
-  lastName: string;
 }
 
 const UserSchema: Schema = new Schema(
   {
     username: String,
     password: String,
-    firstName: String,
-    lastName: String,
   },
   { timestamps: true }
 );
